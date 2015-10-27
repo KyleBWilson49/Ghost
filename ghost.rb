5b85b514fa7681dd3ee94ba28062c0825fbb32fe
kylebwilson49 <kylebwilson49@gmail.com>
2015-10-26 22:32:20 -0700

    It's Working

--- ghost.rb
+++ ghost.rb
@@ -1,4 +1,5 @@
-require_relative "dictionary.txt"
+require 'byebug'
+require_relative 'player'
 
 class Ghost
   attr_reader :player1, :player2, :fragment, :current_player, :previous_player, :dictionary
@@ -6,31 +7,52 @@ class Ghost
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @fragment
-    @dictionary = File.readlines('dictionary.txt') { |line| line.chomp }
+    @fragment = ""
+    @dictionary = File.readlines('dictionary.txt').map(&:chomp)
     @current_player = player1
     @previous_player = player2
   end
 
   def play_round
-    take_turn(@current_player)
+    puts "The current fragment is: #{@fragment}"
+    take_turn
     next_player!
   end
 
   def play
+    # debugger
+    play_round until game_over?
+  end
+
+  def game_over?
+    @dictionary.include?(@fragment)
   end
 
   def next_player!
     @current_player, @previous_player = @previous_player, @current_player
   end
 
-  def take_turn(player)
+  def take_turn
+    #debugger
+    player_guess = @current_player.guess
+    until valid_play?(player_guess)
+      take_turn
+    end
+    @fragment << player_guess
   end
 
-  def valid_play(string)
+  def valid_play?(string)
+    guess = @fragment + string
     @dictionary.each do |word|
-      return true if word[0..string.length - 1] == string
+      return true if word[0...guess.length] == guess
     end
     false
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  jane = Player.new('jane')
+  fred = Player.new('fred')
+  game = Ghost.new(jane, fred)
+  game.play
 end
